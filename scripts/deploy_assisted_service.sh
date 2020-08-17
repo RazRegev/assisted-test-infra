bb60edfe9572fc3c58f0c7c948c8b0e4f4d038e5
Raz <rregev@redhat.com>
2020-08-17 23:57:37 +0300

    wip

--- scripts/deploy_assisted_service.sh
+++ scripts/deploy_assisted_service.sh
@@ -9,7 +9,6 @@ export PROFILE=${PROFILE:-assisted-installer}
 export NAMESPACE=${NAMESPACE:-assisted-installer}
 export SERVICE_URL=$(get_main_ip)
 export SERVICE_START_PORT=${SERVICE_START_PORT:-6000}
-export SERVICE_BASE_URL="http://${SERVICE_URL}:${SERVICE_PORT}"
 
 mkdir -p build
 
@@ -21,6 +20,7 @@ print_log "Wait till ${SERVICE_NAME} api is ready"
 wait_for_url_and_run "$(minikube service ${SERVICE_NAME} --url -p $PROFILE -n ${NAMESPACE})" "echo \"waiting for ${SERVICE_NAME}\""
 
 export SERVICE_PORT=$(search_for_next_free_port $SERVICE_NAME $NAMESPACE $SERVICE_START_PORT)
+export SERVICE_BASE_URL="http://${SERVICE_URL}:${SERVICE_PORT}"
 
 print_log "Starting port forwarding for deployment/${SERVICE_NAME} on port $SERVICE_PORT"
 wait_for_url_and_run ${SERVICE_BASE_URL} "spawn_port_forwarding_command ${SERVICE_NAME} ${SERVICE_PORT} $NAMESPACE $PROFILE"
